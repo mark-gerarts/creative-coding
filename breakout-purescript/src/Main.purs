08a872329fbad07442c1f9b070ecd3513110b163
Mark Gerarts <mark.gerarts@gmail.com>
2019-07-10 22:13:16 +0200

    Getting closed to working sketch

--- breakout-purescript/src/Main.purs
+++ breakout-purescript/src/Main.purs
@@ -2,15 +2,41 @@ module Main where
 
 import Prelude
 
-import Effect (Effect, foreachE)
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(..), maybe)
+import Effect (Effect)
 import Effect.Console (log)
+import Graphics.Canvas (rect, stroke)
+import P5 (P5, draw, getP5, setup)
+import P5.Color (background3)
+import P5.Rendering (createCanvas)
+import P5.Shape (strokeWeight)
+import Web.HTML (window)
+import Web.HTML.Window (innerHeight, innerWidth)
 
-main :: Effect Unit
-main = do
-  log "Hello sailor!"
+type AppState = {
+  p5 :: P5
+}
 
-testFst :: forall a. a -> String
-testFst _ = "Hello world"
+initialState :: Maybe AppState
+initialState = Nothing
 
-testSnd :: forall b. b -> Int
-testSnd _ = 5
+main :: Maybe AppState -> Effect (Maybe AppState)
+main mAppState = do
+  win <- window
+  w <- toNumber <$> innerWidth win
+  h <- toNumber <$> innerHeight win
+  p <- maybe getP5 (\x -> pure x.p5) mAppState
+
+  setup p do
+    _ <- createCanvas p w h Nothing
+    _ <- background3 p "#FF00FF" Nothing
+    pure unit
+
+  draw p do
+    background3 p "#EEE" Nothing
+    stroke p "#000"
+    strokeWeight p 5.0
+    rect p 100.0 100.0 50.0 50.0 Nothing Nothing
+    rect p 110.0 110.0 50.0 50.0 Nothing Nothing
+    pure unit
