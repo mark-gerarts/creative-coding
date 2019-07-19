f413c56c278e31ab8a7ac2ecd5c341e1a0fea88c
Mark Gerarts <mark.gerarts@gmail.com>
2019-07-19 19:22:57 +0200

    Draw some defined objects

--- purescript/src/breakout/Breakout.purs
+++ purescript/src/breakout/Breakout.purs
@@ -12,61 +12,67 @@ import P5 (P5, draw, getP5, setup)
 import P5.Color (background3, fill, noStroke, stroke)
 import P5.Rendering (createCanvas)
 import P5.Shape (ellipse, rect, strokeWeight)
-import Vector (Vector)
+import Vector (Vector, add)
 import Web.HTML (window)
+import Web.HTML.HTMLObjectElement (height)
+import Web.HTML.HTMLProgressElement (position)
 import Web.HTML.Window (innerWidth, innerHeight)
 
 type AppState = {
   p5 :: P5,
-  ball :: Ball
-  {- paddle :: Paddle,
-  bricks :: List Brick -}
+  ball :: Ball,
+  paddle :: Paddle
+  -- bricks :: List Brick
 }
 
 data Color = Primary | Secondary
 
 type Position = Vector
 
-newtype Brick = Brick {
+data Brick = Brick {
   position :: Position,
   width :: Number,
   height :: Number,
   color :: Color
 }
 
-newtype Paddle = Paddle {
+data Paddle = Paddle {
   position :: Position,
   width :: Number,
   height :: Number,
   color :: Color
 }
 
-newtype Ball = Ball {
+data Ball = Ball {
   position :: Position,
   radius :: Number,
-  color :: Color
+  color :: Color,
+  a :: Vector
 }
+
+instance showBall :: Show Ball where
+  show (Ball ball) = "Position: " <> show ball.position <> ", a: " <> show ball.a
 
 class Draw a where
   drawObject :: P5 -> a -> Effect Unit
 
 instance drawBrick :: Draw Brick where
-  drawObject p brick = do
-    noStroke
-    fill (getHex brick.color)
-    rect p brick.position.x brick.position.y brick.width brick.height
+  drawObject p (Brick brick) = do
+    noStroke p
+    fill p (getHex brick.color)
+    rect p brick.position.x brick.position.y brick.width brick.height Nothing Nothing
 
 instance drawPaddle :: Draw Paddle where
-  drawObject p paddle = do
-    noStroke
-    fill (getHex paddle.color)
-    rect p paddle.position.x paddle.position.y paddle.width paddle.weight 
+  drawObject p (Paddle paddle) = do
+    noStroke p
+    fill p (getHex paddle.color)
+    rect p paddle.position.x paddle.position.y paddle.width paddle.height Nothing Nothing
 
 instance drawBall :: Draw Ball where
-  drawObject p ball = do
-    noStroke
-    (fill . getHex) ball.color
-    ellipse ball.position.x ball.position.y ball.radius ball.radius
+  drawObject p (Ball ball) = do
+    noStroke p
+    fill p (getHex ball.color)
+    ellipse p ball.position.x ball.position.y ball.radius (Just ball.radius)
 
 getHex :: Color -> String
 getHex c = case c of
@@ -74,24 +80,48 @@ getHex c = case c of
   Secondary -> "#eeeeee"
 
 initialState :: Maybe AppState
-initialState = Just {
-  p5: getP5,
-  ball: Ball { position: {x: 10.0, y: 10.0}, radius: 5.0, color: Primary}
+initialState = Nothing 
+
+initialBall :: Number -> Number -> Ball
+initialBall gameWidth gameHeight = Ball { 
+  position: {x: gameWidth / 2.0, y: gameHeight - 150.0 }, 
+  radius: 20.0, 
+  color: Primary,
+  a: { x: 20.0, y: 20.0 }
 }
 
+initialPaddle :: Number -> Number -> Paddle
+initialPaddle gameWidth gameHeight = Paddle {
+  position: {x: gameWidth / 2.0, y: gameHeight - 100.0},
+  width: 100.0,
+  height: 20.0,
+  color: Primary
+}
+
+-- @todo: merge this in an updateAppState or something.
+updateBall :: Ball -> Ball
+updateBall (Ball ball) = Ball (ball { position = add ball.position ball.a})
+
+-- @todo: figure out how to update state :(
 main :: Maybe AppState -> Effect (Maybe AppState)
 main mAppState = do
   win <- window
   w <- toNumber <$> innerWidth win
   h <- toNumber <$> innerHeight win
   p <- maybe getP5 (\x -> pure x.p5) mAppState
+  ball <- maybe (pure $ initialBall w h) (\x -> pure x.ball) mAppState
+  paddle <- maybe (pure $ initialPaddle w h) (\x -> pure x.paddle) mAppState
 
   setup p do
     _ <- createCanvas p w h Nothing
-    background3 p (getHex Secondary) Nothing
+    log "Setting up"
     pure unit
 
   draw p do
+    background3 p (getHex Secondary) Nothing
+    drawObject p ball
+    drawObject p paddle
+    log "Drawing"
     pure unit
 
-  pure $ Just { p5: p, ball: mAppState.ball }
+  pure $ Just { p5: p, ball: ball, paddle: paddle}
--- purescript/src/breakout/Vector.purs
+++ purescript/src/breakout/Vector.purs
@@ -1,6 +1,11 @@
 module Vector where
   
+import Prelude
+
 type Vector = {
     x :: Number,
     y :: Number
 }
+
+add :: Vector -> Vector -> Vector
+add v1 v2 = {x: v1.x + v2.x, y: v1.y + v2.y}
